35c4869ce31a3365171c1fd64cdda24891d8e8f5
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2022-12-02 12:22:49 +0300

    Создал склелет приложения

---
+++ src/components/WidgetChat.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./WidgetChat.module.css"
+
+const WidgetChat = () => {
+    return (
+        <div className={styles.main}>
+            <h1>Do you need a help?</h1>
+            <div>
+              You can write here
+            </div>
+        </div>
+    );
+};
+
+export default WidgetChat;
